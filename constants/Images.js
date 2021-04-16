a5e49fdbb33f91c6201cf19b9e51b2015d20fec4
Noel Miller <noel.miller@garten.co>
2021-04-15 19:34:42 -0700

    feature/SPREE-2435 compile errors are gone.
    Still have to contend with errors that are showing up in the browser

--- constants/Images.js
+++ constants/Images.js
@@ -1,7 +1,7 @@
 const Onboarding = 'https://images.unsplash.com/photo-1505995433366-e12047f3f144?fit=crop&w=840&q=80';
 const Pro = 'https://images.unsplash.com/photo-1485796826113-174aa68fd81b?fit=crop&w=840&q=80';
 const Products = {
-  'Accessories': 'https://source.unsplash.com//l1MCA0VyNrk/840x840',
+  Accessories: 'https://source.unsplash.com//l1MCA0VyNrk/840x840',
 };
 
 const Profile = 'https://images.unsplash.com/photo-1512529920731-e8abaea917a5?fit=crop&w=840&q=80';
@@ -23,4 +23,4 @@ export default {
   Profile,
   Viewed,
   Avatar,
-}
+};
